53c23fcd229a0f31bff3c85de706b2fcba92e7ca
Sahil Yadav <sahil9082811@gmail.com>
2026-07-08 17:20:23 +0530

    created folder sturcture for message area

--- clients/src/components/layout/ChatSidebar/ChatSidebar.jsx
+++ clients/src/components/layout/ChatSidebar/ChatSidebar.jsx
@@ -5,7 +5,7 @@ import ChatList from "./ChatList";
 
 const ChatSidebar = () => {
   return (
-    <aside className="w-[440px] h-screen bg-[#f8f8fb] border-r border-gray-200 flex flex-col">
+    <aside className="w-[420px] h-screen bg-[#f8f8fb] border-r border-gray-200 flex flex-col">
       <ChatHeader />
       <SearchBar />
       <RecentChats />
--- clients/src/pages/Home.jsx
+++ clients/src/pages/Home.jsx
@@ -2,11 +2,17 @@ import ChatSidebar from "../components/layout/ChatSidebar/index";
 import Sidebar from "../components/layout/Sidebar/Sidebar"
 const Home = () => {
     return (
-        <div className="flex">
+        <div className="flex gap-2">
 
             <Sidebar />
             <ChatSidebar />
-            <div className="flex-1 bg-gray-50">
+         <div 
+                className="flex-1 bg-cover bg-center bg-no-repeat opacity-20"
+                style={{ 
+                    backgroundImage: `url('./bg2.jpg')`,
+               
+                }}
+            >
                 
             </div>
 
